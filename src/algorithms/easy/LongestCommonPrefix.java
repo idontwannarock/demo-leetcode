5aeb99d736385d3ac025d3d621ab87201a3d7c43
Howard Wang <howard.wang@kyperdata.com>
2020-06-12 09:21:21 +0800

    twitch longest common prefix

--- src/algorithms/easy/LongestCommonPrefix.java
+++ src/algorithms/easy/LongestCommonPrefix.java
@@ -23,31 +23,21 @@ public class LongestCommonPrefix {
     public String longestCommonPrefix(String[] strs) {
         System.out.println(Arrays.toString(strs));
 
-        String longestCommonPrefix = "";
+        String prefix = "";
         if (strs.length > 1) {
-            // start compare with first string
-            String current = strs[0];
-            while (longestCommonPrefix.isBlank() && current.length() > 0) {
-                System.out.println("Current test start: " + current);
-                boolean isMatched = true;
-                for (String str : strs) {
-                    if (str.length() < current.length()) {
-                        current = str + " ";
-                        isMatched = false;
-                        break;
-                    }
-                    isMatched &= str.startsWith(current);
-                }
-                if (isMatched) {
-                    longestCommonPrefix = current;
+            prefix = strs[0];
+            int index = 1;
+            while (index < strs.length && prefix.length() > 0) {
+                System.out.println("current prefix: " + prefix);
+                if (strs[index].startsWith(prefix)) {
+                    index++;
                 } else {
-                    // if not matched, cut off the last character
-                    current = current.substring(0, current.length() - 1);
+                    prefix = prefix.substring(0, prefix.length() - 1);
                 }
             }
         } else if (strs.length == 1) {
-            longestCommonPrefix = strs[0];
+            prefix = strs[0];
         }
-        return longestCommonPrefix;
+        return prefix;
     }
 }
